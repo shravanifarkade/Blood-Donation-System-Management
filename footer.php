0685404e3978e228d16ceeb3f73c7d456d8a7e28
shravanifarkade <155962064+shravanifarkade@users.noreply.github.com>
2025-09-10 00:31:27 +0530

    Web

---
+++ footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background: linear-gradient(90deg, #00B4D8, #48CAE4);
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>COPYRIGHT © 2020<br>
+  Blood Bank & Donation Management
+  <br>
+  ALL RIGHTS RESERVED.
+  </center>
+  </div>
+
+
+</body>
+
+</html>
